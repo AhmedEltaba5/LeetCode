e92a45e00411a2b8cb2012e43dd4a53192c80ff9
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-05-25 23:29:44 +0200

    Time: 21 ms (78.75%), Space: 13.3 MB (65.11%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -4,25 +4,26 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        
-        # we will use mod and integer division
-        
-        if x>= 2**31-1 or x <= (-2)**31:
+        if x>= (2**31)-1 or x <= (-2)**31:
             return 0
         
-        # handle negative
-        negative = 1
-        if x < 0:
+        sign = 1
+        
+        if x<0:
+            sign = -1
             x = x * -1
-            negative = -1
-        result = 0
+        
+        res = 0
         while x:
-            right_digit = x%10
-            result = result*10 + right_digit
-            x = x // 10
-            
-        result = negative * result
-        if result >= 2**31-1 or result <= (-2)**31:
+            res = res*10 + x%10
+            x = x//10
+        
+        res = sign*res
+        
+        if res>= (2**31)-1 or res <= (-2)**31:
             return 0
-        return result
+        
+        return res
+        
+        
         

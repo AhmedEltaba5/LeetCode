6ae119cba08ed90dddf0a67d1fdf5b176118683a
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-05-26 16:40:38 +0200

    Time: 39 ms (24.63%), Space: 13.8 MB (16.86%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -7,21 +7,21 @@ class Solution(object):
         
         # solution with BackTrack
         result = []
-        def backTrack(S=[], left=0, right=0):
+        def backTrack(S='', left=0, right=0):
             if len(S) == 2*n:
                 result.append("".join(S))
                 return
             
             if left<n:
-                S.append("(")
-                backTrack(S, left+1,right)
-                S.pop()
+                #S+="("
+                backTrack(S+"(", left+1,right)
+                #S.pop()
             
             if right<left:
-                S.append(")")
-                backTrack(S,left,right+1)
-                S.pop()
+                #S+=")"
+                backTrack(S+")",left,right+1)
+                #S.pop()
                 
         backTrack()
-        return result
+        return set(result)
         

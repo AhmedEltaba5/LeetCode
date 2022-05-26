29989ecb55479046b612b7c1347facbe83b9f54b
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-05-26 14:14:09 +0200

    Time: 1080 ms (38.32%), Space: 16.7 MB (79.82%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -5,41 +5,31 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        # sort the list
+        res = []
+        
         nums.sort()
-        length = len(nums)
         
-        result = []
-        
-        for i in range(length - 2):
+        for i in range(len(nums)-2):
             
-            if i > 0 and nums[i] == nums[i-1]:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
+            left = i+1
+            right = len(nums)-1
             
-            left = i + 1
-            right = length - 1
-            
-            while left < right:
-                total_sum = nums[i] + nums[left] + nums[right]
-                
-                if total_sum < 0:
-                    left = left + 1
-                elif total_sum > 0:
-                    right = right - 1
+            while left < right and left>i and right<len(nums):
+                sum_nums = nums[i]+nums[left]+nums[right]
+                if sum_nums > 0:
+                    right = right-1
+                elif sum_nums < 0:
+                    left = left+1
                 else:
-                    # append triple
-                    result.append([nums[i], nums[left], nums[right]])
-                    
-                    while left < right and nums[left] == nums[left + 1]:
-                        left = left + 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right = right - 1
-                        
-                    left = left + 1
-                    right = right - 1
-                    
-    
-        return result
-                    
-        
+                    # sum equals zero
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left]==nums[left+1]:
+                        left = left+1
+                    while left < right and nums[right]==nums[right-1]:
+                        right = right-1
+                    left = left+1
+                    right = right-1
+        return res
         

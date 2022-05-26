b49e6698f211aa8091cc37667bd6ea8cd4638876
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-05-26 13:37:16 +0200

    Time: 37 ms (28.69%), Space: 13.7 MB (62.79%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -8,13 +8,10 @@ class Solution(object):
         res = ""
         min_str = min(strs, key=len)
         for i in range(len(min_str)):
-            tmp = 1
             for j in strs:
                 if j[i] != min_str[i]:
-                    tmp = 0
-            if tmp:
-                res = res + min_str[i]
-            else:
-                break
+                    return res
+            res = res + min_str[i]
+         
         return res
         

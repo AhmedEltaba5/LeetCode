d47441194ae56e891796a93183554c164132e67c
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-05-26 14:38:05 +0200

    Time: 29 ms (27.29%), Space: 13.6 MB (15.21%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -5,27 +5,30 @@ class Solution(object):
         :rtype: List[str]
         """
         
-        # BackTracking and recursion
+        map_dict = dict()
+        map_dict['2'] = "abc"
+        map_dict['3'] = "def"
+        map_dict['4'] = "ghi"
+        map_dict['5'] = "jkl"
+        map_dict['6'] = "mno"
+        map_dict['7'] = "pqrs"
+        map_dict['8'] = "tuv"
+        map_dict['9'] = "wxyz"
         
-        map_dict = {"1": "", "2": "abc", "3": "def", "4":"ghi", "5":"jkl",
-                   "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        
-        result = []
-        
+        res = []
         def backTrack(i, curStr):
-            # base condition'
             if len(curStr) == len(digits):
-                result.append(curStr)
+                res.append(curStr)
                 return
             
             for char in map_dict[digits[i]]:
                 backTrack(i+1, curStr+char)
-            
-        if digits:
-            backTrack(0, "")
-        
-        return result
                 
-    
-            
+        if len(digits)>0:
+            backTrack(0, "")
+        return res
+                
+        
+        
+        
         

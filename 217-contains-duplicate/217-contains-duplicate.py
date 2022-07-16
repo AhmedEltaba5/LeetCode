f6d0c0e1a8bd9d24c5933431157078d44bbdfe0a
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-07-16 11:23:01 +0200

    Time: 648 ms (29.90%), Space: 24.1 MB (8.69%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -5,10 +5,21 @@ class Solution(object):
         :rtype: bool
         """
         
+        dict={}
+        k=0
+        for item in nums:
+            if item not in dict:
+                dict[item]=k
+                k += 1
+            else:
+                return True
+        return False
+        
+        """
         unique_nums = set(nums)
         
         if len(unique_nums) == len(nums):
             # no duplicates
             return False
         return True
-        
+        """

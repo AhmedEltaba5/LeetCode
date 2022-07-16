ec844e0e8be84aa395bb256b3d9e71b9738d0921
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-07-16 11:25:10 +0200

    Time: 952 ms (5.06%), Space: 23.9 MB (41.23%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -4,23 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        
-        dict_lst=[]
-        k=0
-        for item in nums:
-            if item not in dict_lst:
-                dict_lst.append(item)
-                #dict[item]=k
-                k += 1
-            else:
-                return True
-        return False
-        
-        """
+    
         unique_nums = set(nums)
         
         if len(unique_nums) == len(nums):
             # no duplicates
             return False
         return True
-        """
+        

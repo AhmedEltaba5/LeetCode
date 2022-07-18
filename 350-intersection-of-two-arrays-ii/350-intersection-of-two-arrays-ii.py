f040c342da77a90f8749cce1837b63e32f9b8121
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-07-18 16:48:27 +0200

    Time: 100 ms (9.35%), Space: 13.5 MB (57.78%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -5,20 +5,12 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        nums1_set = set(nums1)
-        nums2_set = set(nums2)
-        
-        res = []
-        if len(nums1_set)<=len(nums2_set):
-            # loop over nums1_set
-            for i in nums1_set:
-                if i in nums2_set:
-                    for _ in range(min(nums1.count(i), nums2.count(i))):
-                        res.append(i)
-        else:
-            # loop over nums2_set
-            for i in nums2_set:
-                if i in nums1_set:
-                    for _ in range(min(nums1.count(i), nums2.count(i))):
-                        res.append(i)
-        return res
+        a = []
+        k=len(nums1)
+        for i in range(k):
+            if(nums1[i] in nums2):
+                a.append(nums1[i])
+                ind = nums2.index(nums1[i])
+                nums2[ind]= '_'
+
+        return a

afc8efa3134ca3c16847ae95de581c915d129c14
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-07-28 18:41:02 +0200

    Time: 37 ms (13.59%), Space: 13.4 MB (63.19%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -20,17 +20,6 @@ class Solution(object):
             out.append(tmp)
         
         return out
-    
-        """
-        if len(out) <= 2:
-            return out
-        
-        for i in range(2,len(out)):
-            for j in range(1,len(out[i])-1):
-                out[i][j] = out[i-1][j-1]+out[i-1][j]
-                
-        return out
-        """
         
         
         

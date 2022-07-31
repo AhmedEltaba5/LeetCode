8a0ebaeb073bd0626862ea0c41ca694481592107
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-07-31 23:45:46 +0200

    Time: 74 ms (42.65%), Space: 15.4 MB (15.38%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -5,7 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
         
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0)+1
+            countT[t[i]] = countT.get(t[i], 0)+1
+            
+        for i in countS:
+            if countS[i] != countT.get(i):
+                return False
+        return True
+        
+        """
         if len(s) != len(t):
             return False
         
@@ -13,4 +26,5 @@ class Solution(object):
             if s.count(i) != t.count(i):
                 return False
         return True
+        """
         

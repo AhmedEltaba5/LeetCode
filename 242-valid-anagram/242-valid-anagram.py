671645e0bc3daf87ceb96de10d982ceaa626c185
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-07-31 23:27:17 +0200

    Time: 34 ms (95.65%), Space: 14.1 MB (57.37%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(s) != len(t):
+            return False
+        
+        for i in set(s):
+            if s.count(i) != t.count(i):
+                return False
+        return True

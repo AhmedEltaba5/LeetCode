b2947019366e9eedb8737f3c21d5e10d26de2d8e
Ahmed Eltabakh <53350601+AhmedEltaba5@users.noreply.github.com>
2022-11-22 00:36:34 +0200

    Time: 791 ms (98.38%), Space: 24.6 MB (45.13%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -12,10 +12,13 @@ class Solution(object):
         dec = True
         for i in range(len(A)-1):
             if A[i] > A[i+1]:
-                inc = False
-                
-            if A[i] < A[i+1]:
+                inc = False  
+            elif A[i] < A[i+1]:
                 dec = False
-                
+            
+            # check if monotone case was broken
+            if not (inc or dec):
+                return False
+
         return inc or dec
         
